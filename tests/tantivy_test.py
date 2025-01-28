b23b583982207ed03eaa1daaebf7a85ac342b1ee
Kevin Hu <kevinhuwest@gmail.com>
2025-01-28 14:35:01 -0800

    Bump pyo3 and pythonize versions

--- tests/tantivy_test.py
+++ tests/tantivy_test.py
@@ -680,7 +680,8 @@ class TestDocument(object):
         orig.add_float("float", 1.0)
         orig.add_date("birth", datetime.datetime(2019, 8, 12, 13, 0, 5))
         orig.add_text("title", "hello world!")
-        orig.add_json("json", '{"a": 1, "b": 2}')
+        # note that pythonize will convert positive i64s to u64s, which we don't test here: https://github.com/quickwit-oss/tantivy-py/pull/401#issuecomment-2577733829
+        orig.add_json("json", '{"a": 1.0, "b": -2}')
         orig.add_bytes("bytes", b"abc")
 
         facet = tantivy.Facet.from_string("/europe/france")
